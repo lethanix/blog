06757190dbabd05b9d5792ac751e95596bb9bb41
lethanix <developer@lethanix.com>
2025-04-08 16:37:55 -0600

    Change license format

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,12 +16,10 @@ import { getLinkTarget } from '@/utils/link'
         <span v-if="index < siteConfig.footer.navLinks.length - 1" op-70> / </span>
       </template>
     </div>
+    <span op-70><a nav-link href="https://astro.build/themes/details/vitesse-theme-for-astro/" target="_blank">Theme</a> made by {{ siteConfig.themeAuthor }} and modified by Lethanix.</span>
     <div flex>
-      <a nav-link href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>
-      <span op-70>&nbsp;&nbsp;&copy;&nbsp;&nbsp;{{ new Date().getFullYear() }}&nbsp;&nbsp;{{ siteConfig.author
-      }}.</span>
+      <a nav-link href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" class="text-xsm">CC BY-NC-SA 4.0</a>
+      <span op-70>&nbsp;&nbsp;&copy;&nbsp;&nbsp;{{ new Date().getFullYear() }}&nbsp;&nbsp;</span>
     </div>
-    <span op-70><a nav-link href="https://astro.build/themes/details/vitesse-theme-for-astro/" target="_blank">Theme</a> made by {{ siteConfig.themeAuthor
-    }}.</span>
   </footer>
 </template>
