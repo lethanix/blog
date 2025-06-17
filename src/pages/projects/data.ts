ab62be41db19ffe93f4d2e98251b0d2eaea18fb6
lethanix <me@lethanix.com>
2025-06-16 23:52:33 -0600

    Change title section for smaller screens: Command Line Tools to CLI Tools

--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -43,7 +43,7 @@ export const projectData: ProjectData = [
     // ],
   },
   {
-    title: 'Command Line Tools',
+    title: 'CLI Tools',
     projects: [
       {
         text: 'Log Archive Tool',
